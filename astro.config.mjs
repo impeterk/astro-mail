94f5049a300ee218ad4df504dee4881764e957df
Peter <storm.sk123@gmail.com>
2025-01-11 15:48:37 +0100

    hot-realod-not-working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import mjml from 'vite-plugin-mjml'
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [mjml({
+            input: 'src/templates/mjml',
+            output: 'src/templates/html',
+            extension: '.html',
+            watch: true
+        })]
+    },
+    output: 'server'
+});
