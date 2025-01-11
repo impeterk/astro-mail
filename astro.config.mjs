ff343a1de48f351fad4fe3d2cc754a7d8b3a0d81
peterkudelas@gmail.com <peterkudelas@gmail.com>
2025-01-11 20:37:29 +0100

    🎉finish initial version🎉
    add daisyui
    add layout
    add desktop/mobile switch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 import mjml from 'vite-plugin-mjml'
 // https://astro.build/config
 export default defineConfig({
     vite: {
-        plugins: [mjml({
-            input: 'src/templates/mjml',
-            output: 'src/templates/html',
-            extension: '.html',
-            watch: true
-        })]
+        plugins: [
+            tailwindcss(),
+            mjml({
+                input: 'src/templates/mjml',
+                output: 'src/templates/html',
+                extension: '.html',
+                watch: true
+            })]
     },
     output: 'server'
 });
